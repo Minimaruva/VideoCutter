bdd8a889cf9435b581f6c952c407f17f928e0f9d
Minimaruva <vladjagnion@gmail.com>
2024-10-16 20:56:01 +0100

    basic working image combiner

--- to_video.py
+++ to_video.py
@@ -3,6 +3,8 @@ from moviepy.video.fx.resize import resize
 from quote_img import create_text_image
 import os, sys
 from PIL import Image
+from quote_img import create_text_image
+
 
 def img_resizer(img_path, base_width=1080):
     """Resize image to have same width as video"""
@@ -17,7 +19,21 @@ def img_resizer(img_path, base_width=1080):
 
 
 
-def img_to_img(bg_path="./assets/bg.png", img_path="./assets/Calm/Vagabond1.jpg"):
+def img_to_img(quote_path, img_path="./assets/Calm/Vagabond1.jpg", output_path="./output"):
     """Function that accepts input image to produce formatted image
     This is more efficient then producing a video out of image"""
+    
+    # Create a black background
+    bg = Image.new("RGB", (1080, 1920), "black")
 
+    img = Image.open(img_path)
+    quote = Image.open(quote_path)
+
+    bg.paste(img, (0,250))
+    bg.paste(quote, (0,0))
+
+    bg.save("./output/output_img.png")
+
+quote = create_text_image(text="Trying out long quote bla bla bla. Like really long quote for real man")
+
+img_to_img(quote_path=quote)
